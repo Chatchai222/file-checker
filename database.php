ee8751b25dd6a4dd861c5fcb32c71bf318c9bd99
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2022-07-22 16:40:45 +0700

    Added database.php

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+
+class Database{
+
+    const DB_HOST = "localhost";
+    const DB_USER = "filecheckeradmin";
+    const DB_PASSWORD = "1234";
+    const DB_NAME = "filechecker";
+
+
+}
+
+?>
